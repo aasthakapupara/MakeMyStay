d69e74e7ad751571e88b106e95565d993f1639d6
Aastha Kapupara <aasthakapupara15@gmail.com>
2025-07-04 19:43:27 +0530

    Delete server directory

--- server/routes/Contribute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { contributePostController } from "../controller/Contribute.js";
-
-const app = express();
-
-app.post("/contribute-post", contributePostController);
-
-export default app;
