7a2e71be2d9d14f392b80737454ad1bc83a98ab9
Aastha Kapupara <aasthakapupara15@gmail.com>
2025-07-04 19:44:39 +0530

    Add files via upload

---
+++ server/src/config/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dlj5rwtlv",
+  api_key: "697244821239825",
+  api_secret: "SwA0vMzoaOiMUY3Le_ZlyzP9msE",
+});
+
+export default cloudinary;
